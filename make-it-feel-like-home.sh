54b84e10b8f0866a36ce1bff6c1aa0a6e7a571f1
Sergio <66286679+SergioInToronto@users.noreply.github.com>
2020-12-23 16:05:24 -0500

    Update make-it-feel-like-home.sh

--- make-it-feel-like-home.sh
+++ make-it-feel-like-home.sh
@@ -1,5 +1,6 @@
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 28
 gsettings set org.gnome.desktop.peripherals.keyboard delay 280
+# note: In Ubuntu 20.04 this doesn't work. Go to Settings -> Universal Access -> Repeat Keys -> Adjust the sliders in the GUI :'(
 
 sudo apt install gnome-tweaks -y
 # swap esc with capslock
