b559dd585209eab2daae683a883d51429d8fbf5b
Sergio <66286679+SergioInToronto@users.noreply.github.com>
2021-06-05 13:55:49 -0400

    Update make-it-feel-like-home.sh
    Add mkv thumbnails to nautilus and other goodies

--- make-it-feel-like-home.sh
+++ make-it-feel-like-home.sh
@@ -2,11 +2,17 @@ gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 28
 gsettings set org.gnome.desktop.peripherals.keyboard delay 280
 # note: In Ubuntu 20.04 this doesn't work. Go to Settings -> Universal Access -> Repeat Keys -> Adjust the sliders in the GUI :'(
 
+sudo apt install gimp vim vlc wget curl httpie
+
+# Install closed-source bits like MSCore fonts and thumbnail generators for non-open formats like MP4/mkv
+sudo apt install -y ubuntu-restricted-extras
+
 sudo apt install gnome-tweaks -y
 # swap esc with capslock
 
-# Uninstall totem, amazing, gnome accounts, etc.
+# Install Steam
+pushd /tmp/ && wget https://cdn.akamai.steamstatic.com/client/installer/steam.deb && dpkg -i ./steam.deb && popd
 
-# Set VLC as default for all file types
+# Uninstall totem, amazon, gnome accounts, etc.
 
 # Now find a nice wallpaper
