2de03f5c24f3dc119496a54f824f9dc99c1aec45
Sergio <smartins@movableink.com>
2025-09-09 14:20:58 -0400

    Format

--- rotate-video.py
+++ rotate-video.py
@@ -1,16 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-   rotate_video.py
+rotate_video.py
 
-    A (simple) library to rotate movies.
+ A (simple) library to rotate movies.
 
-    source: https://github.com/laurentperrinet/photoscripts/blob/master/rotate_video.py
+ source: https://github.com/laurentperrinet/photoscripts/blob/master/rotate_video.py
 
 """
 
-
 import sys, os, glob
+
 
 def rotate(PATH, CW=False):
     """
@@ -19,16 +19,20 @@ def rotate(PATH, CW=False):
     2 = 90CounterClockwise
     3 = 90Clockwise and Vertical Flip
     """
-    EXT = PATH.split('.')[-1]
-#     print 'DEBUG: # of transpose = ', str(1+int(CW))
-    cmd = 'ffmpeg  -i "%s" -vf "transpose=%s" -c:a copy -y "%s-tmp.%s" && mv "%s-tmp.%s" "%s"' % (PATH, str(1 + int(CW)), PATH, EXT, PATH, EXT, PATH)
-    print ('DEBUG: cmd = ', cmd)
+    EXT = PATH.split(".")[-1]
+    #     print 'DEBUG: # of transpose = ', str(1+int(CW))
+    cmd = (
+        'ffmpeg  -i "%s" -vf "transpose=%s" -c:a copy -y "%s-tmp.%s" && mv "%s-tmp.%s" "%s"'
+        % (PATH, str(1 + int(CW)), PATH, EXT, PATH, EXT, PATH)
+    )
+    print("DEBUG: cmd = ", cmd)
     try:
         os.system(cmd)
     except Exception as e:
-        print ('Command ', cmd, ' failed, error is: ', e)
+        print("Command ", cmd, " failed, error is: ", e)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     args = sys.argv[1:]
 
     if not len(args):
@@ -42,10 +46,10 @@ if __name__=="__main__":
     else:
         CW = args[0]
         PATHS = args[1:]
-        if CW != '-c':
-            CW = ''
+        if CW != "-c":
+            CW = ""
             PATHS = args
         for PATH in PATHS:
             for filename in glob.glob(PATH):
-                print ('Processing file ', filename)
-                rotate(filename, CW=='-c')
+                print("Processing file ", filename)
+                rotate(filename, CW == "-c")
