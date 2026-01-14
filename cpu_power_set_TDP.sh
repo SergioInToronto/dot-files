84cc5491962063d0aeeaf6e8df38d9afa22aff82
Sergio <fake@unreal.com>
2026-01-14 12:46:09 -0500

    Accept TDP in watts as argument

--- cpu_power_set_TDP.sh
+++ cpu_power_set_TDP.sh
@@ -2,6 +2,11 @@
 
 set -e
 
-# sudo powercap-set intel-rapl-mmio -z 0 -c 1 -l 10500000
-sudo powercap-set intel-rapl-mmio -z 0 -c 1 -l 9500000
+# Default to 10.5 watts
+tdp_in_watts=${1:-10.5}
+
+# powercap uses picowatts. Bash doesn't support floating-point math - use bc
+picowatts=$(echo "${tdp_in_watts} * 10^6 / 1" | bc)
+
+sudo powercap-set intel-rapl-mmio -z 0 -c 1 -l ${picowatts}
 sudo powercap-info intel-rapl-mmio -z 0 -c 1 -l
